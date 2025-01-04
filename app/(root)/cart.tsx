a0f407e468fb19b818e4c3d4fb596d22ae35fc4b
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-01-04 13:19:39 -0600

    fixes

--- app/(root)/cart.tsx
+++ app/(root)/cart.tsx
@@ -6,14 +6,14 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { icons } from "@/constants";
 import { router } from "expo-router";
 import CartCard from "@/components/CartCard";
 import { cartStore } from "@/store/cartStore";
 import CustomButton from "@/components/CustomButton";
-import { StripeProvider, useStripe } from "@stripe/stripe-react-native";
+import { StripeProvider, usePaymentSheet } from "@stripe/stripe-react-native";
 import { useApi } from "@/lib/api";
 import { useUser } from "@clerk/clerk-expo";
 import { useLocationStore } from "@/store/locationStore";
@@ -21,10 +21,10 @@ import { useLocationStore } from "@/store/locationStore";
 const Cart = () => {
   const api = useApi();
   const { user } = useUser();
+  const [ready, setReady] = useState(false);
   const { cart, clearCart } = cartStore();
-  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const { initPaymentSheet, presentPaymentSheet, loading } = usePaymentSheet();
   const { userLatitude, userLongitude } = useLocationStore();
-  // const [success, setSuccess] = useState(false);
 
   const total = Object.values(cart).reduce(
     (acc, { price, quantity }) => acc + price * quantity,
@@ -32,66 +32,49 @@ const Cart = () => {
   );
 
   const initializePaymentSheet = async () => {
-    const { error } = await initPaymentSheet({
-      merchantDisplayName: "Mealify Inc.",
-      intentConfiguration: {
-        mode: {
-          amount: total * 100,
-          currencyCode: "mxn",
-        },
-        confirmHandler: async (paymentMethod, _, intentCreationCallback) => {
-          const { data } = await api.post("/payments/payment-intent", {
-            name:
-              user?.fullName ||
-              user?.emailAddresses[0].emailAddress!.split("@")[0],
-            email: user?.emailAddresses[0].emailAddress!,
-            amount: total,
-            restaurant: Object.values(cart)[0].restaurant_id,
-            userLatitude,
-            userLongitude,
-          });
+    try {
+      setReady(false);
+      const { data } = await api.post("/payments/fetch-params", {
+        email: user?.emailAddresses[0].emailAddress!,
+        amount: total,
+        restaurant: Object.values(cart)[0].restaurant_id,
+        userLatitude,
+        userLongitude,
+        clerkId: user?.id,
+        cart,
+      });
+      const { paymentIntent, ephemeralKey, customer } = data;
 
-          const { customer, paymentIntent: paymentIntentResp } = data;
-          const { paymentIntent } = paymentIntentResp;
+      const { error } = await initPaymentSheet({
+        customerId: customer,
+        customerEphemeralKeySecret: ephemeralKey,
+        paymentIntentClientSecret: paymentIntent,
+        merchantDisplayName: "Mealify Inc.",
+        returnURL: "myapp://book-ride",
+      });
 
-          const { client_secret } = paymentIntent;
-
-          if (client_secret) {
-            const { data } = await api.post("/payments/pay", {
-              payment_method_id: paymentMethod.id,
-              payment_intent_id: paymentIntent.id,
-              customer_id: customer,
-              cart,
-              userLatitude,
-              userLongitude,
-            });
-            const { result, orderId } = data;
-            if (result.client_secret) {
-              // Add to DB
-              intentCreationCallback({
-                clientSecret: result.client_secret,
-              });
-
-              clearCart();
-              router.push(`/(root)/(map-followup)/${orderId}`);
-            }
-          }
-        },
-      },
-      returnURL: "myapp://book-ride",
-    });
+      if (error) {
+        Alert.alert(`${error.code}`, error.message);
+      } else {
+        setReady(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const openPaymentSheet = async () => {
-    await initializePaymentSheet();
     const { error } = await presentPaymentSheet();
-
     if (error) {
       Alert.alert(`${error.code}`, error.message);
     } else {
-      // setSuccess(true);
+      setReady(false);
     }
   };
+
+  useEffect(() => {
+    initializePaymentSheet();
+  }, [total]);
 
   return (
     <StripeProvider
@@ -132,8 +115,8 @@ const Cart = () => {
         />
         <View className="w-10/12 mx-10">
           <CustomButton
-            title={`Pagar $${total}`}
-            disabled={Object.values(cart).length === 0}
+            title="Pagar"
+            disabled={Object.values(cart).length === 0 || loading || !ready}
             onPress={openPaymentSheet}
           />
         </View>
